c08bc1c83497835ffcb686bf70a5c0de224afc7e
弘扬 <hongyang.why@alibaba-inc.com>
2019-09-26 21:38:33 +0800

    Demo应用

---
+++ spring-boot-demo-api/src/main/java/com/spring/boot/demo/api/SpringBootDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApiApplication.class, args);
+    }
+
+}
